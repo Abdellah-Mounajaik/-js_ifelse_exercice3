d12aff0e79b828cef21d70efcc17288b7005ffe1
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:16:28 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+// exo1
+
+// let tabPrenoms = ["abdel", "zak", "ayhan", "marwan", "mohamed" ]
+// let tabLength = tabPrenoms.length
+// console.log(tabLength);
+// console.log(`Bonjour ${tabPrenoms[0]}`);
+// console.log(`Bonjour ${tabPrenoms[1]}`);
+// console.log(`Bonjour ${tabPrenoms[2]}`);
+// console.log(`Bonjour ${tabPrenoms[3]}`);
+// console.log(`Bonjour ${tabPrenoms[4]}`);
+
+// exo2
+
+// let chiffre = 5;
+// let question = prompt("trouve le nombre entre 1 et 10")
+// let reponse = question
+// if (reponse != chiffre) {
+//     prompt("rater, trouve le nombre entre 1 et 10")
+// } else if(reponse == chiffre) {
+//     alert(`Bravo c'était bien ${reponse}`)
+// }
+
+// exo3
+
+// partie2
+
+// let reponse = prompt("Bonjour comment vas-tu ?");
+// console.log(reponse);
+
+// let question = prompt('Comment vas-tu ?');
+// alert("Super je suis content que tu ailles bien");
+
+// let temps = prompt("quelle temps fait-il aujourd'hui?")
+// let sol = "soleil"
+// let ven = "vent"
+// let plui = "pluie"
+// let nej = "neige"
+// if (temps == sol) {
+//     console.log("Sortez en t-shirt");
+// } else if (temps == ven) {
+//     console.log("Sortez en pull");
+// } else if (temps == plui) {
+//     console.log("Mettez une veste");
+// } else if (temps == nej) {
+//     console.log("Reste chez toi, il fait froid");
+// } else {
+//     console.log("je n’ai pas compris");
+// }
